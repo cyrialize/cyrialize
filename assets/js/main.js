041ff8a94928beb13ddaec780566a734e2cad06b
cyrialize <jonathancyr@fastmail.com>
2020-04-28 10:15:37 -0400

    Modified JS so that click to expand images will not be enabled for tablets and phones

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,6 +12,11 @@ function generateModal(modalId, modalClass) {
     return modal;
 }
 
+/**
+ * Creates the pop-up image to be used in the modal
+ * 
+ * @param {*} img 
+ */
 function generatePopUpImg(img) {
     let newImg = img.cloneNode();
     newImg.classList.remove('content-img');
@@ -20,27 +25,35 @@ function generatePopUpImg(img) {
     return newImg;
 }
 
-let imgs = document.getElementsByClassName('content-img');
-for (let i = 0; i < imgs.length; i++) {
-    let modalId = imgs[i].id + '-modal';
-    let modal = generateModal(modalId, 'img-modal');
+
+// Only enable click to expand for images on viewports bigger than tablets and phones
+var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+if (width > 961) {
+    let imgs = document.getElementsByClassName('content-img');
+    for (let i = 0; i < imgs.length; i++) {
+        let modalId = imgs[i].id + '-modal';
+        let modal = generateModal(modalId, 'img-modal');
 
 
-    let innerImg = generatePopUpImg(imgs[i]);
+        let innerImg = generatePopUpImg(imgs[i]);
 
-    imgs[i].onclick = function() {
-        modal.style.display = "block";
-        console.log(imgs[i]);
+        imgs[i].onclick = function() {
+            modal.style.display = "block";
+            console.log(imgs[i]);
+        }
+
+        innerImg.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        modal.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        modal.appendChild(innerImg);
+        document.body.appendChild(modal);
     }
-
-    innerImg.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    modal.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    modal.appendChild(innerImg);
-    document.body.appendChild(modal);
 }
